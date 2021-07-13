f9f3ae101762f8ebf6bf9b59ee0f3f1e681d86b5
Maroban <dbtmdqja207@naver.com>
2021-07-13 15:03:42 +0900

    07/13 메소드 사용

--- src/com/javaex/Controller/PhoneController.java
+++ src/com/javaex/Controller/PhoneController.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.javaex.dao.PhoneDao;
+import com.javaex.util.WebUtil;
 import com.javaex.vo.PersonVo;
 
 @WebServlet("/pbc") // 이 주소가 있어야 연결이 된다.
@@ -33,16 +34,23 @@ public class PhoneController extends HttpServlet {
 			// request.settAttribute()에 데이터를 넣기.
 			request.setAttribute("pList", personList);
 
-			// Controller와 list.jsp 포워드 하기.
-			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/list.jsp");
-			rd.forward(request, response);
+			// Controller와 list.jsp 포워드 하기.(원본)
+			// RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/list.jsp");
+			// rd.forward(request, response);
+
+			// Controller와 list.jsp 포워드 하기.(메소드 사용)
+			WebUtil.forward(request, response, "/WEB-INF/list.jsp");
 
 		} else if ("wform".equals(action)) {
 			System.out.println("[글쓰기 폼]");
 
-			// Controller와 writeForm.jsp 포워드 하기.
-			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/writeForm.jsp");
-			rd.forward(request, response);
+			// Controller와 writeForm.jsp 포워드 하기.(원본)
+			// RequestDispatcher rd =
+			// request.getRequestDispatcher("/WEB-INF/writeForm.jsp");
+			// rd.forward(request, response);
+
+			// Controller와 writeForm.jsp 포워드 하기.(메소드 사용)
+			WebUtil.forward(request, response, "/WEB-INF/writeForm.jsp");
 
 		} else if ("insert".equals(action)) {
 			System.out.println("[저장]");
@@ -59,8 +67,11 @@ public class PhoneController extends HttpServlet {
 			PhoneDao phoneDao = new PhoneDao();
 			phoneDao.insert(personVo);
 
-			// 리다이렉트
-			response.sendRedirect("/phonebook2/pbc?action=list");
+			// 리다이렉트(원본)
+			// response.sendRedirect("/phonebook2/pbc?action=list");
+
+			// 리다이렉트(메소드 사용)
+			WebUtil.redirect(request, response, "/phonebook2/pbc?action=list");
 
 		} else if ("uform".equals(action)) {
 
@@ -74,9 +85,13 @@ public class PhoneController extends HttpServlet {
 			// request.settAttribute()에 데이터를 넣기.
 			request.setAttribute("pVo", personVo);
 
-			// Controller와 updateForm.jsp 포워드 하기.
-			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/updateForm.jsp");
-			rd.forward(request, response);
+			// Controller와 updateForm.jsp 포워드 하기.(원본)
+			// RequestDispatcher rd =
+			// request.getRequestDispatcher("/WEB-INF/updateForm.jsp");
+			// rd.forward(request, response);
+
+			// Controller와 updateForm.jsp 포워드 하기.(메소드 사용)
+			WebUtil.forward(request, response, "/WEB-INF/updateForm.jsp");
 
 		} else if ("update".equals(action)) {
 
@@ -93,8 +108,11 @@ public class PhoneController extends HttpServlet {
 			PhoneDao phoneDao = new PhoneDao();
 			phoneDao.update(personVo);
 
-			// 리다이렉트
-			response.sendRedirect("/phonebook2/pbc?action=list");
+			// 리다이렉트(원본)
+			// response.sendRedirect("/phonebook2/pbc?action=list");
+
+			// 리다이렉트(메소드 사용)
+			WebUtil.redirect(request, response, "/phonebook2/pbc?action=list");
 
 		} else if ("delete".equals(action)) {
 
@@ -105,8 +123,8 @@ public class PhoneController extends HttpServlet {
 			PhoneDao phoneDao = new PhoneDao();
 			phoneDao.delete(id);
 
-			// 리다이렉트
-			response.sendRedirect("/phonebook2/pbc?action=list");
+			// 리다이렉트(메소드 사용)
+			WebUtil.redirect(request, response, "/phonebook2/pbc?action=list");
 		}
 
 	}
